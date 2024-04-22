fa0a7cb9485cc33eb58ee1b9da0e2ccc8b08646c
hyunseung <dev.hsyang@gmail.com>
2024-04-22 18:41:29 +0900

    fix: vue파일에 ref() api 활용 코드 추가

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,40 @@
-<script setup>
+<template>
+  <div>
+    <h2>반응형 메시지</h2>
+    <p>{{ reactiveMessage }}</p>
+    <button v-on:click="addReactiveMessage">Add Message</button>
+    <h2>일반 메시지</h2>
+    <p>{{ normalMessage }}</p>
+    <button v-on:click="addNormalMessage">Add Message</button>
+  </div>
+</template>
 
+
+<script>
+import { ref } from 'vue';
+
+export default{
+  setup(){
+    const reactiveMessage = ref('Hello Reactive Message');
+    const addReactiveMessage = () => {
+      reactiveMessage.value = reactiveMessage.value + '!';
+    };
+
+    let normalMessage = 'Hello Normal Message';
+    const addNormalMessage = () => {
+      normalMessage = normalMessage + '!';
+    };
+
+    return {
+      reactiveMessage,
+      normalMessage,
+      addReactiveMessage,
+      addNormalMessage,
+    }
+  }
+};
 </script>
 
-<template>
-  <div>Hello Vue3</div>
-</template>
 
 <style>
 
