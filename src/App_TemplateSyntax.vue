8e2bb4588cf745fe0962e723a52e6772cd8e0c67
hyunseung <dev.hsyang@gmail.com>
2024-04-23 15:15:49 +0900

    add: template syntax code

---
+++ src/App_TemplateSyntax.vue
@@ -0,0 +1,63 @@
+<template>
+    <div>
+        <h2>보간법</h2>
+        <p>
+            {{ message }}
+        </p>
+        <p v-once>
+            {{ message }}
+        </p>
+        <button v-on:click="message = message + '!'">
+            Click Me!
+        </button>
+        <hr>
+        <h2>v-html</h2>
+        <p v-html="rawHtml">
+        </p>
+        <hr>
+        <h2>속성 바인딩</h2>
+        <div v-bind:title="dynamicTitle">
+            마우스를 올려보세요!
+        </div>
+        <!-- v-bind 는 자주/많이 사용되기 때문에, v-bind:에서 v-bind를 생략하고 : 으로 단축해서 사용할 수 있다. -->
+        <input type="text" value="vi-binding" :disabled="isInputDisabled"/>
+        <!-- 다중 속성 바인딩 -->
+        <input :="attrs" />
+        <hr>
+        <h2>JavaScript 표현식 사용도 가능합니다.</h2>
+        {{ message.split('').reverse().join('') }}
+        <br>
+        {{ isInputDisabled ? '예' : '아니오'}}
+    </div>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+    setup () {
+        const message = ref('안녕하세요!');
+        const rawHtml = ref('<strong>안녕하세요</strong>');
+        const dynamicTitle = ref('안녕하세요~!~!');
+        const isInputDisabled = ref(true);
+        // 다중 속성 바인딩
+        const attrs = ref({
+            type : "password",
+            value : "1234567",
+            disabled : false,
+        });
+
+        return {
+            message,
+            rawHtml,
+            dynamicTitle,
+            isInputDisabled,
+            attrs,
+        };
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
 
-createApp(App).mount('#app')
-console.log('hello')
+createApp(App).mount('#app');
+console.log('hello, find me on main.js');
