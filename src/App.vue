80b759f284734847088640e75db50a69180ac050
hyunseung <dev.hsyang@gmail.com>
2024-04-23 10:28:26 +0900

    add: App.vue -> setup() code

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <p>
+            {{  counter }}
+        </p>
+        <p>
+            {{  message }}
+        </p>
+        <button @click="increment">
+            increment counter!
+        </button>
+    </div>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+    export default {
+        setup(){
+            const counter = ref(0);
+            const increment = () => {
+                counter.value += 1;
+            };
+
+            const message = ref('hello vue3');
+            
+            return{
+                counter,
+                message,
+                increment,
+            };
+        },
+    };
+</script>
+
+<style lang="scss" scoped>
+
+</style>
