fa829dd10fea231bf9cc1c069762966bf4b257bc
hyunseung <dev.hsyang@gmail.com>
2024-04-26 16:21:07 +0900

    add: readOnly api

---
+++ src/App_L4_ReadOnly.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+import { reactive, readonly } from 'vue';
+export default {
+    setup () {
+        const original = reactive({
+            count: 0,
+        });
+
+        const copy = readonly(original);
+        original.count++;
+
+        console.log('original: ' , original.count);
+        console.log('copy: ' , copy.count); // original.count 와 함께 증감이 된다.
+
+        copy.count++; // readonly 속성이기 때문에 값 변경은 불가하다
+
+        return {
+            original,
+            copy,
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
