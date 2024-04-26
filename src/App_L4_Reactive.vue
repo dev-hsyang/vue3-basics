abe6b319003b92db8af241572112f4db59159603
hyunseung <dev.hsyang@gmail.com>
2024-04-26 15:06:44 +0900

    add: ref() 활용 반응형 primitive

--- src/App_L4_Reactive.vue
+++ src/App_L4_Reactive.vue
@@ -11,11 +11,14 @@ import { reactive } from 'vue';
 export default {
     setup () {
         // 반응형 객체 생성하기 
+        // ref() 와 달리 reactive() 는 객체를 생성한다. 객체변수명 자체는 string 값이기 때문에
+        // 아래의 경우 state 값 자체를 바꾸어도 동적으로 DOM 에 업데이트 되지 않는다.
+        // 그러므로 아래와 같이 객체 내부의 변수를 바꾸어야 한다.
         const state = reactive({
             count : 0,
             deep : {
-                count : 0,
-            },
+                count : 0
+            }
         });
 
         const increment = () => {
@@ -25,12 +28,12 @@ export default {
         const deepIncrement = () => {
             state.deep.count++;
         };
-        
+
         return {
             state,
             increment,
             deepIncrement
-        };
+        };    
     },
 };
 </script>
---
+++ src/App_L4_Ref.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <p>
+            {{ message }}
+        </p>
+        <button v-on:click="addMessage">add message</button>
+    </div>
+</template>
+
+<script>
+import { ref }  from 'vue';
+export default {
+    setup () {
+        // ref()는 primitive 타입 변수를 반응형으로 만들 때 사용한다.
+        let message = ref('Hello World!');
+
+        const addMessage = () => {
+            // script 에서 ref타입 반응형 변수의 값에 접근할 때는 .value 를 통해 접근해야 한다.
+            // template 에서는 자동으로 unwrapping 되어 변수명으로 값 접근이 가능하다.
+            message.value = message.value + '!';
+        };
+
+        console.log(message);
+        console.log('message: ' , message.value);
+        console.log('message type: ' , typeof message.value);
+
+        return {
+            message,
+            addMessage,
+        };
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
