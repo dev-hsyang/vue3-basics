412fa8b5e17a4599392a3f6594e7a9c857463d2c
hyunseung <dev.hsyang@gmail.com>
2024-04-26 14:51:41 +0900

    add: reactive로 반응형객체 생성해보기

---
+++ src/App_L4_Reactive.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <button v-on:click="increment">Count: {{ state.count }}</button>
+        <br>
+        <button v-on:click="deepIncrement">Deep Count: {{ state.deep.count }}</button>
+    </div>
+</template>
+
+<script>
+import { reactive } from 'vue';
+export default {
+    setup () {
+        // 반응형 객체 생성하기 
+        const state = reactive({
+            count : 0,
+            deep : {
+                count : 0,
+            },
+        });
+
+        const increment = () => {
+            state.count++;
+        };
+
+        const deepIncrement = () => {
+            state.deep.count++;
+        };
+        
+        return {
+            state,
+            increment,
+            deepIncrement
+        };
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
